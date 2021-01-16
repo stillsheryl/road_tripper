167c92143c1fc80774a4097705bbab289a6aa217
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-16 15:31:30 -0700

    Update formatting in weather poro

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -5,7 +5,7 @@ class Weather
               :id
 
   def initialize(weather_data)
-    @current_weather = current_weather_hash(weather_data)
+    @current_weather = current_weather_hash(weather_data[:current])
     @daily_weather = daily_weather_for_5_days(weather_data[:daily])
     @hourly_weather = hourly_weather_for_8_hours(weather_data[:hourly])
     @id = nil
@@ -13,16 +13,16 @@ class Weather
 
   def current_weather_hash(weather_data)
     {
-      datetime: DateTime.strptime(weather_data[:current][:dt].to_s,'%s'),
-      sunrise: DateTime.strptime(weather_data[:current][:sunrise].to_s,'%s'),
-      sunset: DateTime.strptime(weather_data[:current][:sunset].to_s,'%s'),
-      temperature: weather_data[:current][:temp],
-      feels_like: weather_data[:current][:feels_like],
-      humidity: weather_data[:current][:humidity],
-      uvi: weather_data[:current][:uvi],
-      visibility: weather_data[:current][:visibility],
-      conditions: weather_data[:current][:weather].first[:description],
-      icon: weather_data[:current][:weather].first[:icon]
+      datetime: DateTime.strptime(weather_data[:dt].to_s,'%s').strftime("%Y/%m/%d %k:%M:%S"),
+      sunrise: DateTime.strptime(weather_data[:sunrise].to_s,'%s').strftime("%Y/%m/%d %k:%M:%S"),
+      sunset: DateTime.strptime(weather_data[:sunset].to_s,'%s').strftime("%Y/%m/%d %k:%M:%S"),
+      temperature: fahrenheit(weather_data[:temp]),
+      feels_like: fahrenheit(weather_data[:feels_like]),
+      humidity: weather_data[:humidity],
+      uvi: weather_data[:uvi],
+      visibility: weather_data[:visibility],
+      conditions: weather_data[:weather].first[:description],
+      icon: weather_data[:weather].first[:icon]
     }
   end
 
@@ -36,5 +36,9 @@ class Weather
     weather_data.first(8).map do |hourly|
       HourlyWeather.new(hourly)
     end
+  end
+
+  def fahrenheit(temp)
+    (temp - 273.15) * (9 / 5) + 32
   end
 end
--- spec/poros/weather_spec.rb
+++ spec/poros/weather_spec.rb
@@ -7,16 +7,22 @@ describe 'Weather PORO' do
 
     weather = Weather.new(weather_info)
 
-    expect(weather.datetime).to eq('Sat, 16 Jan 2021 02:36:20 +0000')
-    expect(weather.sunrise).to eq('Fri, 15 Jan 2021 13:18:48 +0000')
-    expect(weather.sunset).to eq('Fri, 15 Jan 2021 23:31:51 +0000')
-    expect(weather.temperature).to eq(277.06)
-    expect(weather.feels_like).to eq(272.03)
-    expect(weather.humidity).to eq(42)
-    expect(weather.uvi).to eq(0)
-    expect(weather.visibility).to eq(10000)
-    expect(weather.conditions).to eq('clear sky')
-    expect(weather.icon).to eq('01n')
+    expect(weather).to be_an_instance_of(Weather)
+
+    expect(weather.current_weather).to be_a(Hash)
+
+    current_weather = weather.current_weather
+
+    expect(current_weather[:datetime]).to eq('2021/01/16  2:36:20')
+    expect(current_weather[:sunrise]).to eq('2021/01/15 13:18:48')
+    expect(current_weather[:sunset]).to eq('2021/01/15 23:31:51')
+    expect(current_weather[:temperature]).to eq(35.910000000000025)
+    expect(current_weather[:feels_like]).to eq(30.879999999999995)
+    expect(current_weather[:humidity]).to eq(42)
+    expect(current_weather[:uvi]).to eq(0)
+    expect(current_weather[:visibility]).to eq(10000)
+    expect(current_weather[:conditions]).to eq('clear sky')
+    expect(current_weather[:icon]).to eq('01n')
 
     expect(weather.daily_weather).to be_an(Array)
     expect(weather.daily_weather.count).to eq(5)
