d2aa146fbb2f628e92d089e6bb3cea0ee6778fe4
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-17 16:12:32 -0700

    Add weather at time method and spec to facade

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -13,4 +13,29 @@ describe "Weather Facade" do
     expect(results.daily_weather).to be_an(Array)
     expect(results.hourly_weather).to be_an(Array)
   end
+
+  it "creates a hash with trip information" do
+    params = {
+      origin: "Denver,CO",
+      destination: "Pueblo,CO"
+    }
+    city_coordinates = {
+      :lat=>39.738453,
+      :long=>-104.984853
+    }
+    driving_time = [6500, "01:44"]
+    results = WeatherFacade.weather_at_time(params, city_coordinates, driving_time)
+
+    expect(results).to be_a(Hash)
+    expect(results).to have_key(:temperature)
+    expect(results[:temperature]).to be_a(Float)
+    expect(results).to have_key(:conditions)
+    expect(results[:conditions]).to be_a(String)
+    expect(results).to have_key(:travel_time)
+    expect(results[:travel_time]).to be_a(String)
+    expect(results).to have_key(:start_city)
+    expect(results[:start_city]).to be_a(String)
+    expect(results).to have_key(:end_city)
+    expect(results[:end_city]).to be_a(String)
+  end
 end
