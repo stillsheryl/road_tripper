6a2056c62ecd5fa45ee7f1158199b7e4f2af8a2b
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-18 10:20:06 -0700

    Update facade to pass error message if invalid city

--- spec/facades/geocoding_facade_spec.rb
+++ spec/facades/geocoding_facade_spec.rb
@@ -11,4 +11,15 @@ describe "Geocoding Facade" do
     expect(results[:lat]).to be_a(Float)
     expect(results[:long]).to be_a(Float)
   end
+
+  it "returns an error if an invalid city" do
+    params = {
+      location: "hfysavew"
+    }
+    response = GeocodingFacade.coordinates(params[:location])
+
+    expect(response).to be_a(Hash)
+    expect(response[:message]).to eq("Unknown Location: hfysavew")
+    expect(response[:status]).to eq(400)
+  end
 end
