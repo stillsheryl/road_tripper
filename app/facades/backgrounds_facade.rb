682b0cc3694f3a7208e22f7875f98b27a93a216d
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-19 19:21:56 -0700

    Refactor facade

--- app/facades/backgrounds_facade.rb
+++ app/facades/backgrounds_facade.rb
@@ -9,23 +9,19 @@ class BackgroundsFacade
       city_coordinates[:lat] = coordinates[:results].first[:locations].first[:latLng][:lat]
       city_coordinates[:long] = coordinates[:results].first[:locations].first[:latLng][:lng]
 
-      if city_coordinates[:status] == 400
-        current_weather = ""
+      weather_info = WeatherService.get_weather(city_coordinates)
+
+      if weather_info[:status] == 400
+        weather_info
       else
-        weather_info = WeatherService.get_weather(city_coordinates)
+        weather = Weather.new(weather_info)
+        current_weather = weather.current_weather[:conditions]
+        photo_info = PhotoService.get_photo(params, current_weather)
 
-        if weather_info[:status] == 400
-          weather_info
+        if photo_info[:total] != 0
+          Photo.new(photo_info, params)
         else
-          weather = Weather.new(weather_info)
-          current_weather = weather.current_weather[:conditions]
-          photo_info = PhotoService.get_photo(params, current_weather)
-
-          if photo_info[:total] != 0
-            Photo.new(photo_info, params)
-          else
-            { error: 'Please provide a valid city for the search.', status: 400 }
-          end
+          { error: 'Please provide a valid city for the search.', status: 400 }
         end
       end
     end
