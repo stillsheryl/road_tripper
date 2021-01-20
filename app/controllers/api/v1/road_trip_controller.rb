db40770ef55b5716cde4183a492dc6675f41c173
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-20 15:08:52 -0700

    Update controller to user roadtrip facade

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,22 +1,13 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     if User.find_by(api_key: params[:api_key]) != nil
-      driving_time = DirectionsFacade.directions_time(params)
-
-      city_coordinates = GeocodingFacade.coordinates(params[:destination])
-
-      weather = WeatherFacade.weather_at_time(params, city_coordinates, driving_time)
-
-      roadtrip = Roadtrip.new(weather)
+      roadtrip = RoadTripFacade.roadtrip_data(params)
 
       output = RoadtripSerializer.new(roadtrip).to_json
 
       render json: output
     else
-      error_message = {
-        error: 'A valid API key is required.',
-        status: 401
-      }
+      error_message = { error: 'A valid API key is required.', status: 401 }
 
       render json: error_message, status: 401
     end
