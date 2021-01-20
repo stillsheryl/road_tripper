b65f25e866b7c875eb2d74e083a8c83819ec76ca
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-19 19:43:41 -0700

    Refactor facade into smaller methods

--- app/facades/backgrounds_facade.rb
+++ app/facades/backgrounds_facade.rb
@@ -1,29 +1,56 @@
 class BackgroundsFacade
   def self.get_photo(params)
-    coordinates = GeocodingService.get_coordinates(params[:location])
-    city_coordinates = {}
+    #get coordinates from params
+    #check coordinates or return error
+    #get weather from coordinates
+    #check weather or return error
+    #get photo from weather and params
+    #check photo exists or return error
 
+  coordinates = GeocodingService.get_coordinates(params[:location])
     if coordinates[:status] == 400
-      coordinates
+      return_error(coordinates)
     else
-      city_coordinates[:lat] = coordinates[:results].first[:locations].first[:latLng][:lat]
-      city_coordinates[:long] = coordinates[:results].first[:locations].first[:latLng][:lng]
-
-      weather_info = WeatherService.get_weather(city_coordinates)
-
-      if weather_info[:status] == 400
-        weather_info
-      else
-        weather = Weather.new(weather_info)
-        current_weather = weather.current_weather[:conditions]
-        photo_info = PhotoService.get_photo(params, current_weather)
-
-        if photo_info[:total] != 0
-          Photo.new(photo_info, params)
-        else
-          { error: 'Please provide a valid city for the search.', status: 400 }
-        end
-      end
+      return_weather_from_coordinates(coordinates, params)
     end
+  end
+
+  def self.return_weather_from_coordinates(coordinates, params)
+    city_coordinates = {}
+    city_coordinates[:lat] = coordinates[:results].first[:locations].first[:latLng][:lat]
+    city_coordinates[:long] = coordinates[:results].first[:locations].first[:latLng][:lng]
+
+    check_weather(city_coordinates, params)
+  end
+
+  def self.check_weather(city_coordinates, params)
+    weather_info = WeatherService.get_weather(city_coordinates)
+
+    if weather_info[:status] == 400
+      return_error(weather_info)
+    else
+      use_weather_to_create_photo(weather_info, params)
+    end
+  end
+
+  def self.use_weather_to_create_photo(weather_info, params)
+    weather = Weather.new(weather_info)
+    current_weather = weather.current_weather[:conditions]
+    photo_info = PhotoService.get_photo(params, current_weather)
+
+    create_photo(photo_info, params)
+  end
+
+  def self.create_photo(photo_info, params)
+    if photo_info[:total] != 0
+      Photo.new(photo_info, params)
+    else
+      error_message = { error: 'Please provide a valid city for the search.', status: 400 }
+      return_error(error_message)
+    end
+  end
+
+  def self.return_error(error_message)
+    error_message
   end
 end
