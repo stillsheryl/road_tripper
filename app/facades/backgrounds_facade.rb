63270ec86ba7602f5ab761916a4d07bc37776386
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-19 20:22:49 -0700

    Refactor and it's looking good!

--- app/facades/backgrounds_facade.rb
+++ app/facades/backgrounds_facade.rb
@@ -1,49 +1,31 @@
 class BackgroundsFacade
   def self.get_photo(params)
     coordinates = GeocodingService.get_coordinates(params[:location])
-    if coordinates[:status] == 400
-      return_error(coordinates)
-    else
-      create_coordinates(coordinates, params)
-    end
+
+    return coordinates if coordinates[:status] == 400
+
+    weather_info = get_weather(coordinates)
+
+    return weather_info if weather_info[:status] == 400
+
+    photo_info = get_photo_info(weather_info, params)
+
+    return { error: 'Please provide a valid city for the search.', status: 400 } if photo_info[:total] == 0
+
+    Photo.new(photo_info, params)  
   end
 
-  def self.create_coordinates(coordinates, params)
+  def self.get_weather(coordinates)
     city_coordinates = {}
     city_coordinates[:lat] = coordinates[:results].first[:locations].first[:latLng][:lat]
     city_coordinates[:long] = coordinates[:results].first[:locations].first[:latLng][:lng]
 
-    check_weather(city_coordinates, params)
+    WeatherService.get_weather(city_coordinates)
   end
 
-  def self.check_weather(city_coordinates, params)
-    weather_info = WeatherService.get_weather(city_coordinates)
-
-    if weather_info[:status] == 400
-      return_error(weather_info)
-    else
-      use_weather_to_create_photo(weather_info, params)
-    end
-  end
-
-  def self.use_weather_to_create_photo(weather_info, params)
+  def self.get_photo_info(weather_info, params)
     weather = Weather.new(weather_info)
     current_weather = weather.current_weather[:conditions]
-    photo_info = PhotoService.get_photo(params, current_weather)
-
-    create_photo(photo_info, params)
-  end
-
-  def self.create_photo(photo_info, params)
-    if photo_info[:total] != 0
-      Photo.new(photo_info, params)
-    else
-      error_message = { error: 'Please provide a valid city for the search.', status: 400 }
-      return_error(error_message)
-    end
-  end
-
-  def self.return_error(error_message)
-    error_message
+    PhotoService.get_photo(params, current_weather)
   end
 end
