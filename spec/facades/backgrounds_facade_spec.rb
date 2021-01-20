84f62aef7f3d5b6598685c2eb9c13039d4fd0e18
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-20 11:58:44 -0700

    Add test for get_weather in facade

--- spec/facades/backgrounds_facade_spec.rb
+++ spec/facades/backgrounds_facade_spec.rb
@@ -10,4 +10,24 @@ describe "Backgrounds Facade" do
     expect(photo).to be_a(Photo)
     expect(photo.image).to be_a(Hash)
   end
+
+  it "retrieves weather for given coordinates", :vcr do
+    coordinates = {:info=>
+      {:statuscode=>0,
+       :messages=>[]},
+     :results=>
+      [{:providedLocation=>{:location=>"Denver,CO"},
+        :locations=>
+         [{
+           :latLng=>{:lat=>39.738453, :lng=>-104.984853},
+           :displayLatLng=>{:lat=>39.738453, :lng=>-104.984853}},
+           {
+       :latLng=>{:lat=>39.738453, :lng=>-104.984853},
+       :displayLatLng=>{:lat=>39.738453, :lng=>-104.984853},
+       }]}]}
+
+    weather = BackgroundsFacade.get_weather(coordinates)
+
+    expect(weather).to be_a(Hash)
+  end
 end
