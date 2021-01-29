572759ecab139e7fa49dcdba01a605db1e6f31dd
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-29 14:54:17 -0700

    Update names for clarity in controllers

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    image = BackgroundsFacade.get_photo(params)
+    image_or_error = BackgroundsFacade.get_photo(params)
 
-    if image.is_a? Photo
-      output = PhotoSerializer.new(image).to_json
+    if image_or_error.is_a? Photo
+      output = PhotoSerializer.new(image_or_error).to_json
       render json: output
 
     else
-      render json: image, status: 400
+      render json: image_or_error, status: 400
     end
   end
 end
--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    weather = ForecastFacade.get_forecast(params)
+    weather_or_error = ForecastFacade.get_forecast(params)
 
-    if weather.is_a? Weather
-      output = WeatherSerializer.new(weather).to_json
+    if weather_or_error.is_a? Weather
+      output = WeatherSerializer.new(weather_or_error).to_json
 
       render json: output
     else
-      render json: weather, status: 400
+      render json: weather_or_error, status: 400
     end
   end
 end
